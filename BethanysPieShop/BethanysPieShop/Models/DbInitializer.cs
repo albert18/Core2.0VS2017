5cdaf46f1854e6b7cffb461048239cddf423338d
albert18 <caps18@hotmail.com>
2018-04-28 16:40:11 -0400

    added core entity framework

---
+++ BethanysPieShop/BethanysPieShop/Models/DbInitializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public static class DbInitializer
+    {
+        public static void Seed(AppDbContext context)
+        {
+            if (!context.Pies.Any())
+            {
+
+            }
+        }
+    }
+}
---
+++ BethanysPieShop/BethanysPieShop/Models/PieRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class PieRepository: IPieRepository
+    {
+        private readonly AppDbContext _appDBContext;
+
+        public PieRepository(AppDbContext appDbContext)
+        {
+            _appDBContext = appDbContext;
+        }
+
+        public IEnumerable<Pie> GetAllPies()
+        {
+            return _appDBContext.Pies;
+        }
+
+        public Pie GetPieById(int pieId)
+        {
+            return _appDBContext.Pies.FirstOrDefault(p => p.Id == pieId);
+        }
+    }
+}
